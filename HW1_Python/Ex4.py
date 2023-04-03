c4141fd56854d7119d073e87fde97e93fcc9fc06
Alikhadempour <ali.khpourarani823@gmail.com>
2023-04-03 20:48:08 +0330

    Solve Ex4

---
+++ HW1_Python/Ex4.py
@@ -0,0 +1,5 @@
+basic_salary = 400
+years_employed = int(input())
+number_of_children = int(input())
+total_salary = 400 + (number_of_children * 20) + (years_employed * 30)
+print(f"Total amount of salary: {total_salary}")
